57478498f8ea942b2b9ad51327e6de60fc36e1a1
jp <jpso.guerreiro@gmail.com>
2021-12-06 17:23:40 -0300

    Add libs

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ let net = require("net")
 let crypto = require("crypto")
 let fs = require("fs")
 let server = net.createServer()
+let serverlib = require("./lib/server")
+server.on('connection', serverlib.main);
 let banner = require("node-banner")
 console.clear()
 
@@ -17,6 +19,7 @@ try {
 }
 let conf = JSON.parse(rawconf)
 //console.log(conf)
+server.listen(conf.port, () => {console.log("Server started")})
 
 }
 
