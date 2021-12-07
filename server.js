4cb5230184a7dc7750d55b33534b384f2d8c56e5
jp <jpso.guerreiro@gmail.com>
2021-12-07 12:26:52 -0300

    .

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ let fs = require("fs")
 let serverlib = require("./lib/server")
 let server = net.createServer(serverlib.main)
 let banner = require("node-banner")
+let prompt = require("prompt")
+let errhandle = require("./lib/errhandle")
+prompt.start()
 
 console.clear()
 
@@ -24,7 +27,11 @@ let conf = JSON.parse(rawconf)
 
 server.listen(conf.port, () => {
     console.log("Server started")
-    
+    // eternal recall of async function
+    prompt.get(["JsRat"],(err, result)=>{
+        if (err) {errhandle.main(err)}
+        console.log("Received input: "+String(result.JsRat))
+    })
 })
     
 
