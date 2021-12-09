8e0dc966902fec7d4bcd433a03b2736542db5d76
Octopus <66487668+J-P-S-O@users.noreply.github.com>
2021-12-09 11:35:04 -0300

    Update main.rs

--- virus/src/main.rs
+++ virus/src/main.rs
@@ -2,5 +2,10 @@ fn main() {
     println!("Hello, world!");
 }
 fn send(){
+    let file = File::open("pass.txt").expect("file not found!");
+    let reader = BufReader::new(file);
+    let lines = reader.lines();
+    
+    let (user, password, email, provider, port) = lines;
     
 }
