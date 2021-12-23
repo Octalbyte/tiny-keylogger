b1a802588c12c0c14ed03c21cecbcab35e8ad7fc
JPSO <jpso.guerreiro@gmail.com>
2021-12-23 17:05:59 +0000

    .

--- script/src/main.rs
+++ script/src/main.rs
@@ -2,12 +2,18 @@ use lettre::{
     transport::smtp::authentication::Credentials, AsyncSmtpTransport, AsyncTransport, Message,
     Tokio1Executor,
 };
-use chrono::prelude::*;
+use chrono::prelude::DateTime;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
 
 fn main() {
     println!("Hello, world!");
 }
-fn send(data: String){
+
+
+
+fn send(data: String) -> Result<String, Error>{
     let file = File::open("pass.txt").expect("file not found!");
     let reader = BufReader::new(file);
     let lines = reader.lines();
@@ -25,17 +31,17 @@ fn send(data: String){
     let subject = format!("{}", &local);
     let body = data.to_string();
 
-    send_email_smtp(&mailer, from, to, subject, body)
+    send_email_smtp(&mailer, from, to, &subject, body)
     
 }
 
-fn send_email_smtp(
+fn send_email_smtp (
     mailer: &AsyncSmtpTransport<Tokio1Executor>,
     from: &str,
     to: &str,
     subject: &str,
     body: String,
-){
+)-> Result<T, E>{
     let email = Message::builder()
         .from(from.parse()?)
         .to(to.parse()?)
