efacfa92b8fd85d34967ac6f0d9d9c471a825215
Octalbyte <octalbyte0o52@protonmail.com>
2022-01-09 16:31:56 -0300

    Use lettre (copy paste lol)

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,43 @@ extern crate lettre_email;
 use lettre::{EmailTransport, SmtpTransport};
 use lettre_email::EmailBuilder;
 use std::path::Path;
+use std::fs:File;
+use lettre::{
+    transport::smtp::authentication::Credentials, AsyncSmtpTransport, AsyncTransport, Message,
+    Tokio1Executor,
+};
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let smtp_credentials =
+        Credentials::new("smtp_username".to_string(), "smtp_password".to_string());
 
+    let mailer = AsyncSmtpTransport::<Tokio1Executor>::relay("smtp.email.com")?
+        .credentials(smtp_credentials)
+        .build();
 
-fn sendmail() {
-    let email = EmailBuilder::new()
-        .to("user@example.org")
-        .from("user@example.com")
-        .subject("Hi, Hello world")
-        .text("Hello world.")
-        .build()
-        .unwrap();
+    let from = "Hello World <hello@world.com>";
+    let to = "42 <42@42.com>";
+    let subject = "Hello World";
+    let body = "<h1>Hello World</h1>".to_string();
 
-    // Open a local connection on port 25
-    let mut mailer = SmtpTransport::builder_unencrypted_localhost().unwrap().build();
-    // Send the email
-    let result = mailer.send(&email);
-
-    if result.is_ok() {
-        println!("Email sent");
-    } else {
-        println!("Could not send email: {:?}", result);
-    }
-
-    assert!(result.is_ok());
+    send_email_smtp(&mailer, from, to, subject, body).await
 }
 
+async fn send_email_smtp(
+    mailer: &AsyncSmtpTransport<Tokio1Executor>,
+    from: &str,
+    to: &str,
+    subject: &str,
+    body: String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let email = Message::builder()
+        .from(from.parse()?)
+        .to(to.parse()?)
+        .subject(subject)
+        .body(body.to_string())?;
 
+    mailer.send(email).await?;
 
-fn main(){
-    sendmail();
+    Ok(())
 }
-
