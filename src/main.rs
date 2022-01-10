d6d13f1a6e627653463bbc4b9b2058833ed80316
JPSO <jpso.guerreiro@gmail.com>
2022-01-10 14:36:37 +0000

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,30 @@
-extern crate lettre;
-extern crate lettre_email;
-
-use lettre::{EmailTransport, SmtpTransport};
+use lettre::smtp::authentication::Credentials;
+use lettre::{SmtpClient, Transport};
+use lettre_email::mime;
 use lettre_email::EmailBuilder;
 use std::path::Path;
-use std::fs::File;
-use lettre::{
-    smtp::authentication::Credentials, AsyncSmtpTransport, AsyncTransport, Message,
-    Tokio1Executor,
-};
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let smtp_credentials =
-        Credentials::new("smtp_username".to_string(), "smtp_password".to_string());
+fn main() {
+    let email = EmailBuilder::new()
+        .to("hello@example.com")
+        .from("me@hello.com")
+        .subject("Example subject")
+        .html("<h1>Hello, world!</h1>")
+        .attachment_from_file(
+            &Path::new("Record.log"), // Path to file on disk
+            Some("records.log"),      // Filename to use in the email
+            &mime::TEXT_PLAIN,
+        )
+        .unwrap()
+        .build()
+        .unwrap();
 
-    let mailer = AsyncSmtpTransport::<Tokio1Executor>::relay("smtp.email.com")?
-        .credentials(smtp_credentials)
-        .build();
+        let mut mailer = SmtpClient::new_simple("smtp.hello.com")
+        .unwrap()
+        .credentials(Credentials::new("username".into(), "password".into()))
+        .transport();
 
-    let from = "Hello World <hello@world.com>";
-    let to = "42 <42@42.com>";
-    let subject = "Hello World";
-    let body = "<h1>Hello World</h1>".to_string();
+       let result = mailer.send(email.into());
 
-    send_email_smtp(&mailer, from, to, subject, body).await
-}
-
-async fn send_email_smtp(
-    mailer: &AsyncSmtpTransport<Tokio1Executor>,
-    from: &str,
-    to: &str,
-    subject: &str,
-    body: String,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let email = Message::builder()
-        .from(from.parse()?)
-        .to(to.parse()?)
-        .subject(subject)
-        .body(body.to_string())?;
-
-    mailer.send(email).await?;
-
-    Ok(())
+        println!("{:?}", result);
 }
